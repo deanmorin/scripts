dfc3453f94206e76179c0a66e779ed847baba7aa
Dean Morin <morin.dean@gmail.com>
2022-05-01 10:23:09 -0700

    Update

--- rmnet0.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "Total usage shown for rmnet0 in /proc/net/dev:  $(adb shell cat /proc/net/dev | grep rmnet0 | tr -dc [\ 0-9] | sumrow)"
--- sumrow.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import string
-import sys
-numbers = sys.stdin.read()
-numbers = string.split(numbers)
-sum = 0
-for n in numbers:
-    sum += int(n)
-print sum
